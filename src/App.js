68c0d3b0e122b5ec519468e2b1c46c502f3c476f
sahilister <sahil.dhiman27091999@gmail.com>
2020-12-10 18:15:24 +0530

    add code for making grid and random food coordinates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // Initiate state
+  state = {
+    rows: 10,
+    cols: 10,
+    grid: [],
+  };
+
+  componentDidMount() {
+    const splice = this.getRandomGrid();
+
+    let grid1 = [];
+    for (let row = 0; row < this.state.rows; row++) {
+      for (let col = 0; col < this.state.cols; col++) {
+        const isSplice = (splice.row === row && splice.col === col);
+        grid1.push({
+          row,
+          col,
+          isSplice,
+        })
+      }
+    }
+
+    this.setState({ grid: grid1 })
+
+  }
+
+  // Gets random grid position
+getRandomGrid() {
+  return {
+    row: Math.floor((Math.random() * this.state.rows)),
+    col: Math.floor((Math.random() * this.state.cols))
+  }
+}
+
+  render () {
+    const gridItems = this.state.grid.map((grid) => {
+      return(
+        <div className="grid-item">
+          <div
+            key={grid.row.toString() + '-' + grid.col.toString()}
+            className={grid.isSplice ? 'is-splice' : ''} >
+          </div>
+        </div>
+      )
+    })
+    return (
+      <div className="container">
+        <div className="grid">{gridItems}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
